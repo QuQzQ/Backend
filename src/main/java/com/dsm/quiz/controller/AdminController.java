028ed4e55c5b4d1f489fa769f3e78d951abe4667
joungeun <lje0818ovo@naver.com>
2021-12-20 19:59:47 +0900

    @Controller -> @RestController

--- src/main/java/com/dsm/quiz/controller/AdminController.java
+++ src/main/java/com/dsm/quiz/controller/AdminController.java
@@ -3,48 +3,31 @@ package com.dsm.quiz.controller;
 import com.dsm.quiz.entity.Quiz;
 import com.dsm.quiz.payload.request.QuizRequest;
 import com.dsm.quiz.service.AdminService;
-import com.dsm.quiz.service.QuizService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class AdminController {
-    private final AdminService adminService;
-    private final QuizService quizService;
 
-    @RequestMapping("/quiz")
-    public String main(){
-        return "thymeleaf/login";
-    }
+    private final AdminService adminService;
 
     @PostMapping("/admin")
-    public String login(HttpServletRequest request){
-        if(adminService.login(request.getParameter("id"), request.getParameter("pw"))){ // 로그인 성공
-            return "thymeleaf/main";
-        }
-        return "thymeleaf/login"; // 로그인 실패
-    }
-
-    @GetMapping("/quiz-page")
-    public String insertPage(){
-        return "thymeleaf/quizInsert";
+    public boolean login(HttpServletRequest request){
+        return adminService.login(request.getParameter("id"), request.getParameter("pw"));
     }
 
     @PostMapping("/quiz")
-    public String quizInsert(QuizRequest quizReq){
+    public void quizInsert(QuizRequest quizReq){
         Quiz quiz = Quiz.builder()
                 .content(quizReq.getContent())
                 .title(quizReq.getTitle())
                 .answer(quizReq.getAnswer())
                 .build();
         adminService.insertQuiz(quiz);
-        return "thymeleaf/main";
     }
 
 }
