1644ca97b6816e564d9849d6eb437903b7476a2a
joungeun <lje0818ovo@naver.com>
2021-12-20 20:21:48 +0900

    QuizController

--- src/main/java/com/dsm/quiz/controller/QuizController.java
+++ src/main/java/com/dsm/quiz/controller/QuizController.java
@@ -20,11 +20,21 @@ import java.util.List;
 public class QuizController {
     private final QuizService quizService;
 
-    @ApiOperation(value = "퀴즈를 리스트 반환")
+    @ApiOperation(value = "퀴즈 리스트 반환 API")
     @ResponseBody
     @GetMapping("quiz/{num}")
     @ApiResponse(code=200, message = "퀴즈 리턴됨", response = QuizResponse.class, responseContainer = "List")
     public List<QuizResponse> quizReturn(@ApiParam(value = "Quiz count", required = true) @PathVariable int num){
         return quizService.randomQuiz(num);
+    }
+
+    @ApiOperation("퀴즈가 채점 API")
+    public boolean quizScore(){
+        return true;
+    }
+
+    @ApiOperation("Point를 더하거나 빼주는 API")
+   public void point(){
+
     }
 }
