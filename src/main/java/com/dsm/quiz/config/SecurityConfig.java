07bc46109d8472f89e5e3b0271e5dd6267a68344
joungeun <lje0818ovo@naver.com>
2021-12-30 11:10:27 +0900

    [ADD] SecurityConfig

---
+++ src/main/java/com/dsm/quiz/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.dsm.quiz.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests() // 이제부터 인증절차에 대한 설정을 진행하겠다
+                    .antMatchers("/**").permitAll()
+                    .antMatchers("/admin/**").authenticated()
+                    .anyRequest().authenticated()
+                .and()
+                .formLogin().disable();
+    }
+}
