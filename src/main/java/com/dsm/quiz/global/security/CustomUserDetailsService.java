894075b1c8d7ee65c5dee1e21ef13fba1dab443b
joungeun <lje0818ovo@naver.com>
2022-01-03 09:28:16 +0900

    [ADD #2] CustomUserDetails and CustomUserDetailsService

---
+++ src/main/java/com/dsm/quiz/global/security/CustomUserDetailsService.java
@@ -0,0 +1,19 @@
+package com.dsm.quiz.global.security;
+
+import com.dsm.quiz.exception.UserNotFoundException;
+import com.dsm.quiz.model.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findById(email)
+                .map(CustomUserDetails::new)
+                .orElseThrow(UserNotFoundException::new);
+    }
+}
