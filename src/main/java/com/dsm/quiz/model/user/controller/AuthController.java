dc3f8533c6a30b9b6a4801ae4c31e2ec3681b4d6
joungeun <lje0818ovo@naver.com>
2022-01-08 23:58:24 +0900

    [ADD] Join Controller

--- src/main/java/com/dsm/quiz/model/user/controller/AuthController.java
+++ src/main/java/com/dsm/quiz/model/user/controller/AuthController.java
@@ -1,18 +1,23 @@
 package com.dsm.quiz.model.user.controller;
 
 import com.dsm.quiz.model.user.dto.UserCreateRequestDto;
+import com.dsm.quiz.model.user.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class AuthController {
+
+    private final AuthService authService;
 
     @PostMapping("/auth")
     @ResponseStatus(HttpStatus.CREATED)
     public void join(UserCreateRequestDto user){
-
+        authService.join(user);
     }
 
     @PostMapping("/user")
